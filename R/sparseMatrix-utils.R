3ea9f88778a7955e597255b128c7e515f6e8c781
Hervé Pagès <hpages.on.github@gmail.com>
2023-04-01 15:57:57 -0700

    The rowsum() and colsum() S4 generics are now defined in the S4Arrays package

--- R/sparseMatrix-utils.R
+++ R/sparseMatrix-utils.R
@@ -111,30 +111,13 @@ RsparseMatrix <- function(dim, i, j, nzvals, dimnames=NULL)
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### rowsum() and colsum() methods
+### rowsum() method for dgCMatrix objects
 ###
-
-compute_ugroup <- function(group, expected_group_len, reorder=TRUE)
-{
-    if (!(is.vector(group) || is.factor(group)))
-        stop(wmsg("'group' must be a vector or factor"))
-    if (length(group) != expected_group_len)
-        stop(wmsg("incorrect length for 'group'"))
-    if (!isTRUEorFALSE(reorder))
-        stop(wmsg("'reorder' must be TRUE or FALSE"))
-    ## Taken from base::rowsum.default().
-    ugroup <- unique(group)
-    if (anyNA(ugroup))
-        warning(wmsg("missing values for 'group'"))
-    if (reorder)
-        ugroup <- sort(ugroup, na.last=TRUE, method="quick")
-    ugroup
-}
 
 .rowsum_dgCMatrix <- function(x, group, reorder=TRUE, na.rm=FALSE)
 {
     stopifnot(is(x, "dgCMatrix"))
-    ugroup <- compute_ugroup(group, nrow(x), reorder)
+    ugroup <- S4Arrays:::compute_ugroup(group, nrow(x), reorder)
     if (!isTRUEorFALSE(na.rm))
         stop(wmsg("'na.rm' must be TRUE or FALSE"))
     group <- match(group, ugroup)
@@ -143,21 +126,6 @@ compute_ugroup <- function(group, expected_group_len, reorder=TRUE)
     dimnames(ans) <- list(as.character(ugroup), colnames(x))
     ans
 }
-
-### The base package provides rowsum() only (as an S3 generic).
-setGeneric("rowsum", signature="x")
-
-setGeneric("colsum", signature="x",
-    function(x, group, reorder=TRUE, ...)
-        standardGeneric("colsum")
-)
-
-setMethod("colsum", "ANY",
-    function(x, group, reorder=TRUE, ...)
-    {
-        t(rowsum(t(x), group, reorder=reorder, ...))
-    }
-)
 
 ### S3/S4 combo for rowsum.dgCMatrix
 rowsum.dgCMatrix <- function(x, group, reorder=TRUE, ...)
