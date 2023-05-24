0d23bd62082507429652f71e4fabb94812d3f3f6
Hervé Pagès <hpages.on.github@gmail.com>
2023-05-23 18:54:41 -0700

    more work on aperm() method for SVT_SparseArray objects

--- R/SparseArray-aperm.R
+++ R/SparseArray-aperm.R
@@ -3,6 +3,28 @@
 ### -------------------------------------------------------------------------
 ###
 
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Transposition
+###
+
+### S3/S4 combo for t.SVT_SparseMatrix
+t.SVT_SparseMatrix <- function(x)
+{
+    new_SVT <- .Call2("C_transpose_SVT_SparseMatrix",
+                      x@dim, x@type, x@SVT, PACKAGE="SparseArray")
+    BiocGenerics:::replaceSlots(x, dim=rev(x@dim),
+                                   dimnames=rev(x@dimnames),
+                                   SVT=new_SVT,
+                                   check=FALSE)
+}
+setMethod("t", "SVT_SparseMatrix", t.SVT_SparseMatrix)
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Multi-dimensional transposition
+###
+
 .transpose_SVT <- function(x)
 {
     stopifnot(is(x, "SVT_SparseArray"))
@@ -13,4 +35,51 @@
                                    SVT=new_SVT,
                                    check=FALSE)
 }
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### aperm()
+###
+
+### Like aperm2() in S4Arrays, extend base::aperm() by allowing dropping
+### and/or adding ineffective dimensions.
+.aperm.COO_SparseArray <- function(a, perm)
+{
+    a_dim <- dim(a)
+    perm <- S4Arrays:::normarg_perm(perm, a_dim)
+    msg <- S4Arrays:::validate_perm(perm, a_dim)
+    if (!isTRUE(msg))
+        stop(wmsg(msg))
+    ans_dim <- a_dim[perm]
+    ans_dim[is.na(perm)] <- 1L
+    ans_nzcoo <- a@nzcoo[ , perm, drop=FALSE]
+    ans_nzcoo[ , is.na(perm)] <- 1L
+    ans_dimnames <- a@dimnames[perm]
+    new_COO_SparseArray(ans_dim, ans_dimnames,
+                        ans_nzcoo, a@nzvals, check=FALSE)
+}
+
+### S3/S4 combo for aperm.COO_SparseArray
+aperm.COO_SparseArray <-
+    function(a, perm, ...) .aperm.COO_SparseArray(a, perm, ...)
+setMethod("aperm", "COO_SparseArray", aperm.COO_SparseArray)
+
+.aperm.SVT_SparseArray <- function(a, perm)
+{
+    a_dim <- dim(a)
+    perm <- S4Arrays:::normarg_perm(perm, a_dim)
+    msg <- S4Arrays:::validate_perm(perm, a_dim)
+    if (!isTRUE(msg))
+        stop(wmsg(msg))
+    if (!identical(perm, rev(seq_along(a_dim))))
+        stop("'perm' not suported yet!")
+    if (length(a_dim) == 2L)
+        return(t.SVT_SparseMatrix(a))
+    .transpose_SVT(a)
+}
+
+### S3/S4 combo for aperm.SVT_SparseArray
+aperm.SVT_SparseArray <-
+    function(a, perm, ...) .aperm.SVT_SparseArray(a, perm, ...)
+setMethod("aperm", "SVT_SparseArray", aperm.SVT_SparseArray)
 
--- src/SparseArray_aperm.c
+++ src/SparseArray_aperm.c
@@ -8,6 +8,316 @@
 #include "SBT_utils.h"
 
 
+/****************************************************************************
+ * 2D transposition
+ */
+
+static void count_nonzero_vals_per_row(SEXP SVT, int nrow, int ncol,
+		int *nzcounts)
+{
+	int j, lv_len, k;
+	SEXP subSVT, lv_offs, lv_vals;
+	const int *p;
+
+	memset(nzcounts, 0, sizeof(int) * nrow);
+	for (j = 0; j < ncol; j++) {
+		subSVT = VECTOR_ELT(SVT, j);
+		if (subSVT == R_NilValue)
+			continue;
+		/* 'subSVT' is a "leaf vector". */
+		lv_len = _split_leaf_vector(subSVT, &lv_offs, &lv_vals);
+		if (lv_len < 0)
+			error("SparseArray internal error in "
+			      "count_nonzero_vals_per_row():\n"
+			      "    invalid SVT_SparseMatrix object");
+		for (k = 0, p = INTEGER(lv_offs); k < lv_len; k++, p++)
+			nzcounts[*p]++;
+	}
+	return;
+}
+
+static void **set_quick_out_vals_p(SEXP out_SVT, SEXPTYPE Rtype)
+{
+	int out_SVT_len, i;
+	SEXP lv;
+
+	out_SVT_len = LENGTH(out_SVT);
+	switch (Rtype) {
+	    case LGLSXP: case INTSXP: {
+		int **vals_p, **p;
+		vals_p = (int **) R_alloc(out_SVT_len, sizeof(int *));
+		for (i = 0, p = vals_p; i < out_SVT_len; i++, p++) {
+			lv = VECTOR_ELT(out_SVT, i);
+			if (lv != R_NilValue)
+				*p = INTEGER(VECTOR_ELT(lv, 1));
+		}
+		return (void **) vals_p;
+	    }
+	    case REALSXP: {
+		double **vals_p, **p;
+		vals_p = (double **) R_alloc(out_SVT_len, sizeof(double *));
+		for (i = 0, p = vals_p; i < out_SVT_len; i++, p++) {
+			lv = VECTOR_ELT(out_SVT, i);
+			if (lv != R_NilValue)
+				*p = REAL(VECTOR_ELT(lv, 1));
+		}
+		return (void **) vals_p;
+	    }
+	    case CPLXSXP: {
+		Rcomplex **vals_p, **p;
+		vals_p = (Rcomplex **) R_alloc(out_SVT_len, sizeof(Rcomplex *));
+		for (i = 0, p = vals_p; i < out_SVT_len; i++, p++) {
+			lv = VECTOR_ELT(out_SVT, i);
+			if (lv != R_NilValue)
+				*p = COMPLEX(VECTOR_ELT(lv, 1));
+		}
+		return (void **) vals_p;
+	    }
+	    case RAWSXP: {
+		Rbyte **vals_p, **p;
+		vals_p = (Rbyte **) R_alloc(out_SVT_len, sizeof(Rbyte *));
+		for (i = 0, p = vals_p; i < out_SVT_len; i++, p++) {
+			lv = VECTOR_ELT(out_SVT, i);
+			if (lv != R_NilValue)
+				*p = RAW(VECTOR_ELT(lv, 1));
+		}
+		return (void **) vals_p;
+	    }
+	}
+	/* STRSXP and VECSXP cases. */
+	return NULL;
+}
+
+typedef void (*TransposeCol_FUNType)(int col_idx,
+		const int *offs, SEXP lv_vals,
+		int **quick_out_offs_p, void **quick_out_vals_p,
+		SEXP out_SVT, int *nzcounts);
+
+/* Ignores 'out_SVT' and 'nzcounts'. */
+static void transpose_INTEGER_col(int col_idx,
+		const int *offs, SEXP lv_vals,
+		int **quick_out_offs_p, void **quick_out_vals_p,
+		SEXP out_SVT, int *nzcounts)
+{
+	int **vals_p;
+	int lv_len, k, row_idx;
+	const int *v;
+
+	vals_p = (int **) quick_out_vals_p;
+	lv_len = LENGTH(lv_vals);
+	for (k = 0, v = INTEGER(lv_vals); k < lv_len; k++, v++) {
+		row_idx = *offs;
+		*(quick_out_offs_p[row_idx]++) = col_idx;
+		*(vals_p[row_idx]++) = *v;
+		offs++;
+	}
+	return;
+}
+
+/* Ignores 'out_SVT' and 'nzcounts'. */
+static void transpose_NUMERIC_col(int col_idx,
+		const int *offs, SEXP lv_vals,
+		int **quick_out_offs_p, void **quick_out_vals_p,
+		SEXP out_SVT, int *nzcounts)
+{
+	double **vals_p;
+	int lv_len, k, row_idx;
+	const double *v;
+
+	vals_p = (double **) quick_out_vals_p;
+	lv_len = LENGTH(lv_vals);
+	for (k = 0, v = REAL(lv_vals); k < lv_len; k++, v++) {
+		row_idx = *offs;
+		*(quick_out_offs_p[row_idx]++) = col_idx;
+		*(vals_p[row_idx]++) = *v;
+		offs++;
+	}
+	return;
+}
+
+/* Ignores 'out_SVT' and 'nzcounts'. */
+static void transpose_COMPLEX_col(int col_idx,
+		const int *offs, SEXP lv_vals,
+		int **quick_out_offs_p, void **quick_out_vals_p,
+		SEXP out_SVT, int *nzcounts)
+{
+	Rcomplex **vals_p;
+	int lv_len, k, row_idx;
+	const Rcomplex *v;
+
+	vals_p = (Rcomplex **) quick_out_vals_p;
+	lv_len = LENGTH(lv_vals);
+	for (k = 0, v = COMPLEX(lv_vals); k < lv_len; k++, v++) {
+		row_idx = *offs;
+		*(quick_out_offs_p[row_idx]++) = col_idx;
+		*(vals_p[row_idx]++) = *v;
+		offs++;
+	}
+	return;
+}
+
+/* Ignores 'out_SVT' and 'nzcounts'. */
+static void transpose_RAW_col(int col_idx,
+		const int *offs, SEXP lv_vals,
+		int **quick_out_offs_p, void **quick_out_vals_p,
+		SEXP out_SVT, int *nzcounts)
+{
+	Rbyte **vals_p;
+	int lv_len, k, row_idx;
+	const Rbyte *v;
+
+	vals_p = (Rbyte **) quick_out_vals_p;
+	lv_len = LENGTH(lv_vals);
+	for (k = 0, v = RAW(lv_vals); k < lv_len; k++, v++) {
+		row_idx = *offs;
+		*(quick_out_offs_p[row_idx]++) = col_idx;
+		*(vals_p[row_idx]++) = *v;
+		offs++;
+	}
+	return;
+}
+
+/* Ignores 'quick_out_vals_p'. */
+static void transpose_CHARACTER_col(int col_idx,
+		const int *offs, SEXP lv_vals,
+		int **quick_out_offs_p, void **quick_out_vals_p,
+		SEXP out_SVT, int *nzcounts)
+{
+	int lv_len, k, row_idx;
+	SEXP out_lv;
+
+	lv_len = LENGTH(lv_vals);
+	for (k = 0; k < lv_len; k++) {
+		row_idx = *offs;
+		*(quick_out_offs_p[row_idx]++) = col_idx;
+		out_lv = VECTOR_ELT(out_SVT, row_idx);
+		_copy_CHARACTER_elt(lv_vals, (R_xlen_t) k,
+			VECTOR_ELT(out_lv, 1), (R_xlen_t) nzcounts[row_idx]++);
+		offs++;
+	}
+	return;
+}
+
+/* Ignores 'quick_out_vals_p'. */
+static void transpose_LIST_col(int col_idx,
+		const int *offs, SEXP lv_vals,
+		int **quick_out_offs_p, void **quick_out_vals_p,
+		SEXP out_SVT, int *nzcounts)
+{
+	int lv_len, k, row_idx;
+	SEXP out_lv;
+
+	lv_len = LENGTH(lv_vals);
+	for (k = 0; k < lv_len; k++) {
+		row_idx = *offs;
+		*(quick_out_offs_p[row_idx]++) = col_idx;
+		out_lv = VECTOR_ELT(out_SVT, row_idx);
+		_copy_LIST_elt(lv_vals, (R_xlen_t) k,
+			VECTOR_ELT(out_lv, 1), (R_xlen_t) nzcounts[row_idx]++);
+		offs++;
+	}
+	return;
+}
+
+static TransposeCol_FUNType select_transpose_col_FUN(SEXPTYPE Rtype)
+{
+	switch (Rtype) {
+	    case LGLSXP: case INTSXP: return transpose_INTEGER_col;
+	    case REALSXP:             return transpose_NUMERIC_col;
+	    case CPLXSXP:             return transpose_COMPLEX_col;
+	    case RAWSXP:              return transpose_RAW_col;
+	    case STRSXP:              return transpose_CHARACTER_col;
+	    case VECSXP:              return transpose_LIST_col;
+	}
+	return NULL;
+}
+
+static SEXP transpose_SVT(SEXP SVT, SEXPTYPE Rtype, int nrow, int ncol,
+		int *nzcounts)
+{
+	TransposeCol_FUNType transpose_col_FUN;
+	SEXP ans, ans_elt, subSVT, lv_offs, lv_vals;
+	int **quick_out_offs_p;
+	void **quick_out_vals_p;
+	int i, j, lv_len;
+
+	transpose_col_FUN = select_transpose_col_FUN(Rtype);
+	if (transpose_col_FUN == NULL)
+		error("SparseArray internal error in "
+		      "transpose_SVT():\n"
+		      "    SVT_SparseMatrix object has invalid type");
+
+	ans = PROTECT(NEW_LIST(nrow));
+	quick_out_offs_p = (int **) R_alloc(nrow, sizeof(int *));
+	for (i = 0; i < nrow; i++) {
+		lv_len = nzcounts[i];
+		if (lv_len != 0) {
+			ans_elt = PROTECT(_alloc_leaf_vector(lv_len, Rtype));
+			SET_VECTOR_ELT(ans, i, ans_elt);
+			UNPROTECT(1);
+			quick_out_offs_p[i] = INTEGER(VECTOR_ELT(ans_elt, 0));
+		}
+	}
+	quick_out_vals_p = set_quick_out_vals_p(ans, Rtype);
+
+	memset(nzcounts, 0, sizeof(int) * nrow);
+	for (j = 0; j < ncol; j++) {
+		subSVT = VECTOR_ELT(SVT, j);
+		if (subSVT == R_NilValue)
+			continue;
+		/* 'subSVT' is a "leaf vector". */
+		lv_len = _split_leaf_vector(subSVT, &lv_offs, &lv_vals);
+		if (lv_len < 0) {
+			UNPROTECT(1);
+			error("SparseArray internal error in "
+			      "transpose_SVT():\n"
+			      "    invalid SVT_SparseMatrix object");
+		}
+		transpose_col_FUN(j,
+			INTEGER(lv_offs), lv_vals,
+			quick_out_offs_p, quick_out_vals_p,
+			ans, nzcounts);
+	}
+	UNPROTECT(1);
+	return ans;
+}
+
+/* --- .Call ENTRY POINT --- */
+SEXP C_transpose_SVT_SparseMatrix(SEXP x_dim, SEXP x_type, SEXP x_SVT)
+{
+	SEXPTYPE Rtype;
+	int x_nrow, x_ncol, *nzcounts;
+
+	Rtype = _get_Rtype_from_Rstring(x_type);
+	if (Rtype == 0)
+		error("SparseArray internal error in "
+		      "C_transpose_SVT_SparseMatrix():\n"
+		      "    SVT_SparseMatrix object has invalid type");
+
+	if (LENGTH(x_dim) != 2)
+		error("object to transpose must have exactly 2 dimensions");
+
+	if (x_SVT == R_NilValue)
+		return x_SVT;
+
+	x_nrow = INTEGER(x_dim)[0];
+	x_ncol = INTEGER(x_dim)[1];
+	nzcounts = (int *) R_alloc(x_nrow, sizeof(int));
+
+	/* 1st pass: Count the number of nonzero values per row in the
+	   input object. */
+	count_nonzero_vals_per_row(x_SVT, x_nrow, x_ncol, nzcounts);
+
+	/* 2nd pass: Build the transposed SVT. */
+	return transpose_SVT(x_SVT, Rtype, x_nrow, x_ncol, nzcounts);
+}
+
+
+/****************************************************************************
+ * Multi-dimensional transposition
+ */
+
+
 static void push_leaf_vector_to_SBT_row(
 		SEXP SBT, const int *dim, int ndim,
 		SEXP lv, int *coords0_buf)
